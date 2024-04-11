c50ebd884570bd436aaaef1143412485c57a0c0c
tinchx1 <108639112+tinchx1@users.noreply.github.com>
2024-04-11 20:47:24 -0300

    add exercice 20-21-22

--- 2ndo/Seminario-kotlin-android/practica-1/Ej19/app/src/main/java/com/example/appej19/MainActivity.kt
+++ 2ndo/Seminario-kotlin-android/practica-1/Ej19/app/src/main/java/com/example/appej19/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.appej19
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +17,16 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val number = intent.getIntExtra("number", 0)
+        val txtContador = findViewById<TextView>(R.id.txtContador)
+        txtContador.text = number.toString()
+    }
+
+    fun handleClick(view: View) {
+        val txtContador = findViewById<TextView>(R.id.txtContador)
+        val number = txtContador.text.toString().toInt()
+        val i = Intent(Intent.ACTION_VIEW)
+        i.putExtra("number", number)
+        startActivity(i)
     }
 }
--- 2ndo/Seminario-kotlin-android/practica-1/Ej19/app/src/main/java/com/example/appej19/MainActivity2.kt
+++ 2ndo/Seminario-kotlin-android/practica-1/Ej19/app/src/main/java/com/example/appej19/MainActivity2.kt
@@ -3,6 +3,9 @@ package com.example.appej19
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.view.View
+import android.widget.Toast
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,6 +14,21 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
+    }
 
+    fun handleClick(view: View) {
+        var number = intent.getIntExtra("number", 0)
+        if (view.id == R.id.btnIncrementar) {
+            number++
+        } else if (view.id == R.id.btnDecrementar) {
+            number--
+        }
+        if (number == -1) {
+            Toast.makeText(this, "Error el numero no puede ser menor a 0", Toast.LENGTH_SHORT).show()
+        } else {
+            val i = Intent(this, MainActivity::class.java)
+            i.putExtra("number", number)
+            startActivity(i)
+        }
     }
 }
