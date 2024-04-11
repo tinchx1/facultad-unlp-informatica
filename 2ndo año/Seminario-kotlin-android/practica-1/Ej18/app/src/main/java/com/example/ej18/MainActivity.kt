2255329c9a75a114b2dd29848433a3ac439884f1
tinchx1 <108639112+tinchx1@users.noreply.github.com>
2024-04-10 21:18:26 -0300

    add kt

---
+++ 2ndo año/Seminario-kotlin-android/practica-1/Ej18/app/src/main/java/com/example/ej18/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.ej18
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun changeLanguage(view: View) {
+        val text = findViewById<TextView>(R.id.text)
+        if (text.text == "Español") {
+            text.text = "Inglés"
+        } else {
+            text.text = "Español"
+        }
+    }
+
+}
