9c989129921e642789bebb1c2906f0a452d22889
Martin Marchionni <tinchomarchi34@gmail.com>
2024-05-09 09:12:33 -0300

    add ej 1 p3 kt

--- 2ndo/Seminario-kotlin-android/practica-2/ej12/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ 2ndo/Seminario-kotlin-android/practica-2/ej12/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var numberRandom: Number;
+    lateinit var dado: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,11 +21,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        dado = findViewById<TextView>(R.id.dado)
     }
     fun handleClick(view: View) {
-        val dado = findViewById<TextView>(R.id.dado)
         val random = Random()
         val number = random.nextInt(6) + 1
+        numberRandom = number
         dado.text = number.toString()
     }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("numberRandom", numberRandom.toInt())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        numberRandom = savedInstanceState.getInt("numberRandom");
+        dado.setText(numberRandom.toString());
+    }
+
 }
