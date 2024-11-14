41d48c177217967a943e90ed7a2bcca840654fe8
tinchx1 <tinchomarchi34@gmail.com>
2024-11-14 12:54:44 -0300

    Add classes for Cliente, Vendedor, Producto, Pedido, and Mercado; refactor CalculadoraDistancia and Persona

---
+++ 2ndo/segundo-cuatri/oob1/ej25/src/main/java/com/example/Mascota.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import java.time.LocalDate;
+
+public class Mascota {
+  private String nombre;
+  private LocalDate fechaNac;
+  private String especio;
+}
---
+++ 2ndo/segundo-cuatri/oob1/ej25/src/main/java/com/example/Veterinario.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import java.time.LocalDate;
+
+public class Veterinario {
+  private String nombre;
+  private LocalDate fechaIng;
+  private double cobra;
+
+}
